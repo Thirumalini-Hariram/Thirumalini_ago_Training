8929bb1a5f99ce04a3158b4093dba9017ea1d5d5
thirumalini.hari@gmail.com <thirumalini.hari@gmail.com>
2023-12-22 06:33:57 -0800

    commited bootstrap project

---
+++ JS/bootstrapproject.js
@@ -0,0 +1,30 @@
+function home(){
+window.alert("Homeclicked");
+}
+
+
+function aboutus(){
+    window.alert("Clicked About Us");
+    }
+    
+
+    function contactus(){
+        window.alert("Clicked Contatct Us");
+        }
+    
+    
+function Link(){
+    window.alert("Clicked Link");
+    }
+    
+    function help(){
+        window.open("https://getbootstrap.com/docs/5.0/components/navbar/");
+        }
+        
+function button(){
+
+    const clickme=document.getElementById("clickme");
+    clickme.innerHTML="<h1>Welcome</h1>";
+}
+
+
